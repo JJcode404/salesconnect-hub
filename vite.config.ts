2d325c9c6cbf4f7c8e6e25c13334066a24de8a8e
JJcode404 <khamjapher1@gmail.com>
2026-03-10 22:48:12 +0300

    fix: add salesconnect orgin to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     // ✅ allow ngrok + localhost
 
     allowedHosts: [
+      "https://org.sales-connect.site",
       "localhost",
       "localhost:8080",
       "localhost:8081",
